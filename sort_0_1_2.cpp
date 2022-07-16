cd3cc1e8241431103b953ed32fa3d14bbfdffff1
LOKESH PANDEY <73874665+lokeshs333@users.noreply.github.com>
2022-07-17 01:14:25 +0530

    Add files via upload

---
+++ sort_0_1_2.cpp
@@ -0,0 +1,28 @@
+// coding ninjas
+#include <bits/stdc++.h>
+void sort012(int *arr, int n)
+{
+    //   Write your code here
+    int low, high, mid;
+    low = 0;
+    mid = 0;
+    high = n - 1;
+    while (mid <= high)
+    {
+        switch (arr[mid])
+        {
+        case 0:
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+            break;
+        case 1:
+            mid++;
+            break;
+        case 2:
+            swap(arr[mid], arr[high]);
+            high--;
+            break;
+        }
+    }
+}
